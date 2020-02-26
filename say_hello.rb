53914c18d894c75032249c4b92d12dab0f0fe79c
impactlearner <time.darden@gmail.com>
2020-02-26 18:46:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -6,7 +6,7 @@ end
 # def greeting(name = "Ruby programmer")
 #   puts "Hello, #{name}"
 # end
-# 
+#
 # def greeting(name="Ruby programmer", language="Ruby")
 #   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 # end
