96a1b2237f5f080c251bf76af5d708b35639759c
impactlearner <time.darden@gmail.com>
2020-02-26 18:48:29 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,4 @@
 # Build your say_hello method here
 def say_hello(name = "Ruby Programmer")
-  puts "Hello, #{name}!"
+  puts "Hello #{name}!"
 end
-
-# def greeting(name = "Ruby programmer")
-#   puts "Hello, #{name}"
-# end
-#
-# def greeting(name="Ruby programmer", language="Ruby")
-#   puts "Hello, #{name}. We heard you are a great #{language} programmer."
-# end
